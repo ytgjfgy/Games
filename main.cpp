3f3c9b13c4881ad0978e8df770abe0902c85b8c6
ytgjfgy <100783045+ytgjfgy@users.noreply.github.com>
2022-03-02 17:56:08 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,90 @@
+#include "TXLib.h"
+
+struct Personage
+{
+    int x;
+    int y;
+    HDC pic;
+    int v;
+};
+
+void drawSpike(Personage spike)
+{
+    Win32::TransparentBlt (txDC(), spike.x, spike.y, 100, 120, spike.pic, 0, 0, 200, 243, TX_WHITE);
+}
+
+int main()
+{
+txCreateWindow (1300, 600);
+
+txDisableAutoPause();
+txTextCursor(false);
+
+Personage dino = {100, 400, txLoadImage ("Картинки/DINOZAVR/DINOZAVR.bmp")};
+
+Personage spike[50];
+spike[0].pic = txLoadImage ("Картинки/SPIKE/SPIKE1.bmp");
+for(int i=0; i<3; i++)
+{
+    spike[i] = {700+(i*300), 400, spike[0].pic, 10};
+}
+
+//HDC REI = txLoadImage ("Картинки/ANIME/REI.bmp");
+HDC DESERT = txLoadImage ("Картинки/FON/DESERT.bmp");
+
+txSetColor (TX_BLACK, 5);
+txSetFillColor (TX_WHITE);
+
+
+    while(!GetAsyncKeyState (VK_ESCAPE))
+    {
+        txClear();
+        txBegin();
+
+        txBitBlt(txDC(),0, 0, 1300, 600, DESERT);
+
+        txLine(0, 525, 1300, 525);
+
+        //Динозавр
+        Win32::TransparentBlt (txDC(), dino.x, dino.y, 100, 125, dino.pic, 0, 0, 200, 253, TX_WHITE);
+
+        if(GetAsyncKeyState(VK_SPACE))
+        {
+         while(GetAsyncKeyState(VK_SPACE) == 1)
+            txSleep(20);
+         dino.v = - 30;
+        }
+        //Гравитация
+        dino.y=dino.y+dino.v;
+        dino.v = dino.v + 5;
+        //Уровень земли
+        if(dino.y>400)
+        dino.y=400;
+
+        if(dino.y<0)
+        dino.y=0;
+
+        //Кактус
+        for(int i=0; i<3; i++)
+        {
+            drawSpike(spike[i]);
+            spike[i].x = spike[i].x - spike[i].v;
+        }
+
+
+
+        txSleep(10);
+        txEnd();
+    }
+
+    txDeleteDC (dino.pic);
+    for(int i=0; i<3; i++)
+    {
+        txDeleteDC (spike[i].pic);
+    }
+    txDeleteDC (DESERT);
+
+return 0;
+}
+
+
